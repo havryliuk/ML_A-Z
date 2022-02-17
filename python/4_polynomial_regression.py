c09c2f3338dcadd43282a925bf1c7ae26a9a47aa
havryliuk <oleksander.havryliuk@gmail.com>
2022-02-17 11:03:19 +0200

    improve polynomial regression plot * display predicted salary values with value

--- python/4_polynomial_regression.py
+++ python/4_polynomial_regression.py
@@ -23,12 +23,18 @@ polynomial_regression_2 = LinearRegression()
 polynomial_regression_2.fit(X_powered_2, y)
 y_power_2_predicted = polynomial_regression_2.predict(X_powered_2)
 
+# calculate predicted salary with linear and polynomial models
+years = 7.5
+salary_level_7_5_linear = linear_regression.predict([[years]])
+salary_level_7_5_poly = polynomial_regression_2.predict(polynomial_features_2.fit_transform([[years]]))
+print(f'Predicted salary for 7.5 years (linear): {salary_level_7_5_linear}')
+print(f'Predicted salary for 7.5 years (polynomial): {salary_level_7_5_poly}')
+
 pyplot.scatter(X, y)
 pyplot.plot(X, y_predicted)
 pyplot.plot(X, y_power_2_predicted)
+pyplot.scatter(years, salary_level_7_5_linear, marker='x', color='red')
+pyplot.scatter(years, salary_level_7_5_poly, marker='x', color='red')
+pyplot.annotate(round(salary_level_7_5_linear[0]), (years, salary_level_7_5_linear))
+pyplot.annotate(round(salary_level_7_5_poly[0]), (years, salary_level_7_5_poly))
 pyplot.show()
-
-salary_level_7_5_linear = linear_regression.predict([[7.5]])
-salary_level_7_5_poly = polynomial_regression_2.predict(polynomial_features_2.fit_transform([[7.5]]))
-print(salary_level_7_5_linear)
-print(salary_level_7_5_poly)
